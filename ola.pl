fb54ac354de6cc1f2a7dbaced1627d6dfd88009a
Cristobal Marchant <cristobal.marchant@usach.cl>
2022-05-02 20:28:54 -0400

    cortar y simbolos listos

--- ola.pl
+++ ola.pl
@@ -1,67 +1,82 @@
 %--------------------------------------------------------------
-c1(1, [1]).
-c1(N, [N|L]):-
-    N>1, 
-    N1 is N-1,
-    c1(N1, L).
+c1(N,M,Ls):-nth1(1,Ls,Xsimbo),N1 is N+1, c1(N,Xsimbo,M,2,N1,Ls).
+
+c1(N,M,[M],N1,N1,Ls).
+c1(N,M,[M|L],K,N1,Ls):-
+    N>=K,
+    nth1(K,Ls,M2),
+    K1 is K+1,
+    c1(N,M2,L,K1,N1,Ls).
 %--------------------------------------------------------------
 op(N, J, K, M):- M is (N*J)+K+1.
 %--------------------------------------------------------------
-c2(N,M,J):-op(N, J, 1, M2),N1 is N+1, c2(N,M2,M,J,2,N1).
+c2(N,M,J,Ls):-op(N, J, 1, M2),nth1(M2,Ls,Xsimbo),N1 is N+1, c2(N,Xsimbo,M,J,2,N1,Ls).
 
-c2(N,M,[M],J,N1,N1).
-c2(N,M,[M|L],J,K,N1):-
+c2(N,M,[M],J,N1,N1,Ls).
+c2(N,M,[M|L],J,K,N1,Ls):-
     N>=K,
     op(N, J, K, M2),
+    nth1(M2,Ls,Xsimbo),
     K1 is K+1,
-    c2(N,M2, L, J, K1,N1).
+    c2(N,Xsimbo, L, J, K1,N1,Ls).
 %--------------------------------------------------------------
-c3(N,R):-c2(N,M,1),N1 is N+1, c3(N,[1|M],R,2,N1).
+c3(N,R,Ls):-c2(N,M,1,Ls),N1 is N+1,nth1(1,Ls,Xsimbo),c3(N,[Xsimbo|M],R,2,N1,Ls).
 
-c3(N,R,[R],N1,N1). 
-c3(N,R,[R|C],J,N1):-
+c3(N,R,[R],N1,N1,Ls). 
+c3(N,R,[R|C],J,N1,Ls):-
     N>=J,
-    c2(N,L,J),
+    c2(N,L,J,Ls),
     J1 is J+1,
-    c3(N,[1|L],C,J1,N1).
+    nth1(1,Ls,Xsimbo),
+    c3(N,[Xsimbo|L],C,J1,N1,Ls).
 %--------------------------------------------------------------
 op2(N,I,J,K,L):- L is (N+2+(N*(K-1))+((((I-1)*(K-1))+J-1) mod N)).
 %--------------------------------------------------------------
-c4(N,I,J,M):-op2(N,I,J,1,M2),N1 is N+1,c4(N,M2,M,I,J,2,N1).
+c4(N,I,J,M,Ls):-op2(N,I,J,1,M2),N1 is N+1,nth1(M2,Ls,Xsimbo),c4(N,Xsimbo,M,I,J,2,N1,Ls).
 
-c4(N,M,[M],I,J,N1,N1).
-c4(N,M,[M|L],I,J,K,N1):-
+c4(N,M,[M],I,J,N1,N1,Ls).
+c4(N,M,[M|L],I,J,K,N1,Ls):-
     N>=K,
     op2(N,I,J,K,M2),
+    nth1(M2,Ls,Xsimbo),
     K1 is K+1,
-    c4(N,M2,L,I,J,K1,N1).
+    c4(N,Xsimbo,L,I,J,K1,N1,Ls).
 %--------------------------------------------------------------
-c5(N,R,I,J,K):-c4(N,I,J,M),N1 is N+1,Iz is I+1,c5(N,[Iz|M],R,I,2,N1,K).
+c5(N,R,I,J,K,Ls):-c4(N,I,J,M,Ls),N1 is N+1,Iz is I+1,nth1(Iz,Ls,Xsimbo),c5(N,[Xsimbo|M],R,I,2,N1,K,Ls).
 
-c5(N,R,[R],I,N1,N1,K). 
-c5(N,R,[R|C],I,J,N1,K):-
+c5(N,R,[R],I,N1,N1,K,Ls). 
+c5(N,R,[R|C],I,J,N1,K,Ls):-
     N>=J,
-    c4(N,I,J,M),
+    c4(N,I,J,M,Ls),
     J1 is J+1,
     Iz is I+1,
-    c5(N,[Iz|M],C,I,J1,N1,K).
+    nth1(Iz,Ls,Xsimbo),
+    c5(N,[Xsimbo|M],C,I,J1,N1,K,Ls).
 %--------------------------------------------------------------
-c6(N,I,J,K,R):- c5(N,M,I,J,K), N1 is N+1,c6(N,2,2,J,N1,M,R).
+c6(N,I,J,K,R,Ls):- c5(N,M,I,J,K,Ls), N1 is N+1,c6(N,2,2,J,N1,M,R,Ls).
 
-c6(N,N1,K,J,N1,R,R).
-c6(N,I,K,J,N1,M,R):-
+c6(N,N1,K,J,N1,R,R,Ls).
+c6(N,I,K,J,N1,M,R,Ls):-
     N>=I,
-    c5(N,M1,I,J,K),
+    c5(N,M1,I,J,K,Ls),
     I1 is I+1,
     append(M,M1,Z),
-    c6(N,I1,K,J,N1,Z,R).
+    c6(N,I1,K,J,N1,Z,R,Ls).
 %--------------------------------------------------------------
-cards(N,R,NC):-
-    c1(N,R1),
+cards(N,R,NC,Ls):-
+    c1(N,R1,Ls),
     N1 is N-1,
-    c3(N1,R2),
-    c6(N1,1,1,1,R3),
+    c3(N1,R2,Ls),
+    c6(N1,1,1,1,R3,Ls),
     append([R1],R2,R4),
     append(R4,R3,R5),
     length(R, NC),				%para cortar las cartas
     append(R, _, R5).
+
+
+
+list_to_set([1,2,2,4],L) %elementos repetidos en una carta
+same_length              %tamaño lista igual
+
+intersection([1,6,3,8,9],[1,2,4,3,5],L)  %1 elemento en comun
+length([],L).
